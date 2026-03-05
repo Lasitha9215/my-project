eb2d8c108f4859cacef78ecdf53aa375f979e407
Themiya <themiyawijenayaka100@gmail.com>
2026-03-06 00:54:47 +0530

    Final Game

--- Guessing_game.py
+++ Guessing_game.py
@@ -2,14 +2,16 @@ import random
 
 number = random.randint(1, 20) 
 guess = 0
+attempts=0
 
 print("Guess a number between 1 and 20")
 
 while guess != number:
     guess = int(input("Your guess: "))
+    attempts += 1
     if guess < number:
         print("Too low!")
     elif guess > number:
         print("Too high!")
 
-print("You guessed it correct! The number was", number)
+print(f"You guessed it correct! The number was {number}. Attempts: {attempts}")
